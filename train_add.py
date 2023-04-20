fe8f22751b6e84d451d1462163865b679ead480f
KJ <kernel1983@gmail.com>
2023-04-20 22:51:45 +0800

    fix tape for RIGHT

--- train_add.py
+++ train_add.py
@@ -24,31 +24,35 @@ class VM(vm.VM):
             else:
                 print(code, self.code, self.tape, self.gas)
 
-host = sys.argv[1]
-secret = 'hello'
+if len(sys.argv):
+    LEFT = 0
+    RIGHT = 1
+    UP = 2
+    DOWN = 3
+    IF = 4
+    GO = 5
+    ZERO = 6
+    ONE = 7
 
-vm = VM()
-while True:
-    timestamp = int(time.time())
-    # timestamp = 1681295648
-    rsp = requests.post('http://%s/add' % host, data=json.dumps({
-        'timestamp': timestamp,
-        'otp': hashlib.sha256((secret+str(timestamp)).encode('utf8')).hexdigest()}))
-    task = rsp.json()['task']
-    test = rsp.json()['test']
-    print(task, test)
-    vm.train(task, test)
+    vm = VM()
+    vm.code = [IF, DOWN, RIGHT, RIGHT, UP, LEFT, LEFT, GO, RIGHT, IF, DOWN, RIGHT, UP, LEFT, GO]
+    vm.run([100, 2, 0])
+    print(vm.tape)
 
 # 22885275 [3, 0, 2, 2, 0, 3, 4, 3, 1, 2] [0, 0, 3] 989
 # 22931931 [3, 0, 2, 2, 0, 3, 5, 3, 1, 2] [0, 0, 3] 989
+else:
+    host = sys.argv[1]
+    secret = 'hello'
 
-LEFT = 0
-RIGHT = 1
-UP = 2
-DOWN = 3
-IF = 4
-GO = 5
-
-vm.code = [IF, DOWN, RIGHT, RIGHT, UP, LEFT, LEFT, GO, RIGHT, IF, DOWN, RIGHT, UP, LEFT, GO]
-vm.run([100, 2, 0])
-print(vm.tape)
+    vm = VM()
+    while True:
+        timestamp = int(time.time())
+        # timestamp = 1681295648
+        rsp = requests.post('http://%s/add' % host, data=json.dumps({
+            'timestamp': timestamp,
+            'otp': hashlib.sha256((secret+str(timestamp)).encode('utf8')).hexdigest()}))
+        task = rsp.json()['task']
+        test = rsp.json()['test']
+        print(task, test)
+        vm.train(task, test)
--- vm.py
+++ vm.py
@@ -8,7 +8,10 @@ UP = 2
 DOWN = 3
 IF = 4
 GO = 5
+ZERO = 6
+ONE = 7
 TOTAL_OP = 6
+DEBUG = True
 
 class VM:
     # def __init__(self, state = []):
@@ -40,34 +43,44 @@ class VM:
                 self.gas -= 1
                 self.step()
         except:
-            pass
+            print('except')
 
 
     def step(self):
         # print('head', self.head, self.tape[self.head])
+        # print('pc', self.pc)
         # print('local_vars', self.local_vars)
         if self.code[self.pc] == LEFT:
-            # print('LEFT')
+            if DEBUG:
+                print('LEFT')
             self.head -= 1
+            if self.head < 0:
+                raise
             self.pc += 1
 
         elif self.code[self.pc] == RIGHT:
-            # print('RIGHT')
+            if DEBUG:
+                print('RIGHT', self.tape, self.head)
             self.head += 1
+            if len(self.tape) >= self.head:
+                self.tape.extend([0]*(self.head - len(self.tape) + 1))
             self.pc += 1
 
         elif self.code[self.pc] == UP:
-            # print('UP')
+            if DEBUG:
+                print('UP')
             self.tape[self.head] += 1
             self.pc += 1
 
         elif self.code[self.pc] == DOWN:
-            # print('DOWN')
+            if DEBUG:
+                print('DOWN')
             self.tape[self.head] -= 1
             self.pc += 1
 
         elif self.code[self.pc] == IF:
-            # print('IF')
+            if DEBUG:
+                print('IF')
             if self.tape[self.head] == 0:
                 while self.code[self.pc] != GO:
                     self.pc += 1
@@ -76,7 +89,8 @@ class VM:
                 self.pc += 1
 
         elif self.code[self.pc] == GO:
-            # print('GO')
+            if DEBUG:
+                print('GO')
             if self.tape[self.head] > 0:
                 while self.code[self.pc] != IF:
                     self.pc -= 1
@@ -90,7 +104,7 @@ class VM:
 # print(VM().parse_code(6))
 # print(VM().parse_code(5))
 
-# vm = VM([0])
-# vm.code = [4, 0, 0, 0, 0, 5]
-# vm.run()
+vm = VM()
+vm.code = [4, RIGHT, UP, 5]
+vm.run([1])
 
